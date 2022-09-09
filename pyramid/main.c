c53b47b8eaac7c3e2f414df0d65a4763ebc2cd58
ayasalah106 <113051464+ayasalah106@users.noreply.github.com>
2022-09-09 23:56:35 +0200

    first-version

--- pyramid/main.c
+++ pyramid/main.c
@@ -20,7 +20,7 @@ int main()
     break;
     }*/
 
-    char arr[5][10]={"    *    ","   ***   ","  *****  "," ******* ","*********"};
+    /*char arr[5][10]={"    *    ","   ***   ","  *****  "," ******* ","*********"};
     printf("how many rows you want:");
     scanf("%d", &n);
     while(n>=2&&n<=5)
@@ -32,7 +32,44 @@ int main()
 
         break;
     }
-    printf("REFUSED");
+    printf("REFUSED");*/
+
+    char arr1[10]= {"    *    "};
+    char arr2[10]= {"   ***   "};
+    char arr3[10]= {"  *****  "};
+    char arr4[10]= {" ******* "};
+    char arr5[10]= {"*********"};
+    printf("Enter height of pyramid : ");
+    scanf("%d",&i);
+    if(i==1)
+        printf("%s\n",arr1);
+    else if(i==2)
+    {
+        printf("%s\n",arr1);
+        printf("%s\n",arr2);
+    }
+    else if(i==3)
+    {
+        printf("%s\n",arr1);
+        printf("%s\n",arr2);
+        printf("%s\n",arr3);
+    }
+    else if(i==4)
+    {
+        printf("%s\n",arr1);
+        printf("%s\n",arr2);
+        printf("%s\n",arr3);
+        printf("%s\n",arr4);
+    }
+    else if(i==5)
+    {
+        printf("%s\n",arr1);
+        printf("%s\n",arr2);
+        printf("%s\n",arr3);
+        printf("%s\n",arr4);
+        printf("%s\n",arr5);
+    }
+
 
 
 }
